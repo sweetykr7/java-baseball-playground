b8219555614421176ef2c54bde1ac655e562303b
sweetykr7 <sweetykr@naver.com>
2024-02-16 09:23:21 +0900

    Test: 게임 계산하는 로직에 대한 테스트를 구현

--- src/test/java/study/BaseBallGameTest.java
+++ src/test/java/study/BaseBallGameTest.java
@@ -1,7 +1,10 @@
 package study;
 
 import baseball.InputView;
+import baseball.ResultGame;
+import baseball.ResultGameDTO;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
 public class BaseBallGameTest {
@@ -11,4 +14,22 @@ public class BaseBallGameTest {
 //        String keyInputValue = inputView.keyInput();
 //        Assertions.assertEquals(keyInputValue, "test");
 //    }
+    @Nested
+    class ResultGameTest{
+
+        @Test
+        void calculateGameTest() {
+            ResultGame resultGame = new ResultGame();
+            ResultGameDTO returnDto = resultGame.calculateGame("123","152");
+            Assertions.assertEquals(1, returnDto.getBall());
+            Assertions.assertEquals(1, returnDto.getStrike());
+
+            ResultGame resultGame2 = new ResultGame();
+            ResultGameDTO returnDto2 = resultGame2.calculateGame("518","351");
+            Assertions.assertEquals(2, returnDto2.getBall());
+            Assertions.assertEquals(0, returnDto2.getStrike());
+        }
+
+    }
+
 }
